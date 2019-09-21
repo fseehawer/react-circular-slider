be761299247ec64a8b1f69d21fa998baad974b5b
Fidel Seehawer <fidel@kreativgebiet.com>
2019-09-21 12:04:44 +0200

    Add basic features

--- src/CircularSlider.js
+++ src/CircularSlider.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useRef } from 'react';
+import React, { useState, useEffect, useLayoutEffect, useCallback, useRef } from 'react';
 import {StyleSheet, css} from 'aphrodite';
 
 const touchSupported = ('ontouchstart' in window);
@@ -18,13 +18,15 @@ const CircularSlider = () => {
             radians: -1,
             x: 191,
             y: 0,
-        }
+        },
+        dashFullArray: 0,
+        dashFullOffset: 0
     });
 
     const pulse_animation = {
-        "0%": {transform: "scale(0.8)"},
-        "50%": {transform: "scale(1)"},
-        "100%": {transform: "scale(0.8)"}
+        "0%": {transform: "scale(1)"},
+        "50%": {transform: "scale(0.8)"},
+        "100%": {transform: "scale(1)"}
     };
 
     const styles = StyleSheet.create({
@@ -36,6 +38,15 @@ const CircularSlider = () => {
             position: 'absolute',
             left: '-16px',
             top: '-16px',
+            cursor: 'grab'
+        },
+
+        drag: {
+            cursor: 'grabbing'
+        },
+
+        pause: {
+            animationPlayState: 'paused',
         },
 
         animation: {
@@ -47,69 +58,8 @@ const CircularSlider = () => {
         }
     });
 
-    let dashOffset = 0;
-    let dashFullOffset = 0;
     let circularSlider = useRef(null);
-    let svgPath = null;
-    let svgFullPath = null;
-    let mouseXFromCenter = 0;
-    let mouseYFromCenter = 0;
-
-    const onMouseDown = useCallback((event) => {
-        setState(prevState => ({
-            ...prevState,
-            isDragging: true
-        }));
-        event.preventDefault();
-
-        document.addEventListener("mousemove", onMouseMove);
-        document.addEventListener("mouseup", onMouseUp);
-    }, []);
-
-    const onMouseMove = useCallback((event) => {
-        console.log('mouse move isDragging', state.isDragging);
-        if(state.isDragging) return;
-        event.stopPropagation();
-        event.preventDefault();
-
-        let touch;
-        if (event.type === 'touchmove') {
-            touch = event.changedTouches[0];
-        }
-
-        mouseXFromCenter = (event.type === 'touchmove' ? touch.pageX : event.pageX) -
-            (offsetRelativeToDocument().left + state.radius);
-        mouseYFromCenter = (event.type === 'touchmove' ? touch.pageY : event.pageY) -
-            (offsetRelativeToDocument().top + state.radius);
-
-        const radians = Math.atan2(mouseYFromCenter, mouseXFromCenter);
-        indicatorPosition(radians);
-        console.log(radians, 'radians')
-    }, []);
-
-    const indicatorPosition = (radians) => {
-        const radius = state.radius;
-
-            setState(prevState => ({
-            ...prevState,
-            indicator: {
-                scale: 1,
-                radians: radians,
-                x: (radius * Math.cos(radians) + radius),
-                y: (radius * Math.sin(radians) + radius),
-            }
-        }));
-    };
-
-    const onMouseUp = (event) => {
-        console.log('mouse up')
-        setState(prevState => ({
-            ...prevState,
-            isDragging: false
-        }));
-        document.removeEventListener("mousemove", onMouseMove);
-        document.removeEventListener("mouseup", onMouseUp);
-    };
+    let svgFullPath = useRef(null);
 
     const offsetRelativeToDocument = useCallback(() => {
         const rect = circularSlider.current.getBoundingClientRect();
@@ -118,18 +68,80 @@ const CircularSlider = () => {
         return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
     }, []);
 
-    let transformStyles = null;
+    const indicatorPosition = useCallback((radians) => {
+        const radius = state.radius;
+        const offsetRadians = radians + 1.5708; // offset by 90 degrees
+        const degrees = (offsetRadians > 0 ? offsetRadians
+            :
+            ((2 * Math.PI) + offsetRadians)) * (360 / (2 * Math.PI));
+
+        const dashOffset = state.dashFullArray - ((degrees / 360) * state.dashFullArray);
+
+        setState(prevState => ({
+            ...prevState,
+            dashFullOffset: dashOffset,
+            indicator: {
+                scale: 1,
+                radians: radians,
+                x: (radius * Math.cos(radians) + radius),
+                y: (radius * Math.sin(radians) + radius),
+            }
+        }));
+    }, [state.dashFullArray, state.radius]);
+
+    const onMouseDown = useCallback((event) => {
+        setState(prevState => ({
+            ...prevState,
+            isDragging: true
+        }));
+        event.preventDefault();
+
+    }, []);
+
+    const onMouseMove = useCallback((event) => {
+        if(!state.isDragging) return;
+        event.stopPropagation();
+        event.preventDefault();
+
+        let touch;
+        if (event.type === 'touchmove') {
+            touch = event.changedTouches[0];
+        }
+
+        const mouseXFromCenter = (event.type === 'touchmove' ? touch.pageX : event.pageX) -
+            (offsetRelativeToDocument().left + state.radius);
+        const mouseYFromCenter = (event.type === 'touchmove' ? touch.pageY : event.pageY) -
+            (offsetRelativeToDocument().top + state.radius);
+
+        const radians = Math.atan2(mouseYFromCenter, mouseXFromCenter);
+        indicatorPosition(radians);
+    }, [state.isDragging, state.radius, indicatorPosition, offsetRelativeToDocument]);
+
+    const onMouseUp = (event) => {
+        setState(prevState => ({
+            ...prevState,
+            isDragging: false
+        }));
+    };
+
+    useLayoutEffect(() => {
+        setState(prevState => ({
+            ...prevState,
+            dashFullArray: svgFullPath.current.getTotalLength(),
+            dashFullOffset: svgFullPath.current.getTotalLength(),
+        }));
+    }, []);
 
     useEffect( () => {
-        dashOffset = svgPath.getTotalLength();
-        dashFullOffset = svgFullPath.getTotalLength();
-
-        console.log(transformStyles, 'transformStyles');
-
-        return () => {
-
+        if(state.isDragging) {
+            document.addEventListener(SLIDER_EVENT.MOVE, onMouseMove);
+            document.addEventListener(SLIDER_EVENT.UP, onMouseUp);
+            return () => {
+                document.removeEventListener(SLIDER_EVENT.MOVE, onMouseMove);
+                document.removeEventListener(SLIDER_EVENT.UP, onMouseUp);
+            }
         }
-    }, [state]);
+    }, [state.isDragging, onMouseMove]);
 
     return (
         <div className={css(styles.circularSlider)} ref={circularSlider}>
@@ -144,63 +156,36 @@ const CircularSlider = () => {
                         <stop offset="100%" stopColor="#80C3F3"/>
                     </linearGradient>
                 </defs>
+                <circle strokeWidth={4} fill="none" stroke="#DDDEFB" cx={195} cy={195} r={state.radius} />
                 <path
-                    ref={(path) => { svgPath = path }}
-                    strokeDasharray={1}
-                    strokeDashoffset={1}
+                    ref={svgFullPath}
+                    strokeDasharray={state.dashFullArray}
+                    strokeDashoffset={state.dashFullOffset}
                     strokeWidth="4"
                     strokeLinecap="round"
                     fill="none"
                     stroke="url(#gradient)"
                     d="
-                M 195, 195
-                m 0, -193
-                a 193,193 0 0,1 0,386
-            "/>
-                <path
-                    strokeDasharray={1}
-                    strokeDashoffset={1}
-                    strokeWidth="4"
-                    strokeLinecap="round"
-                    fill="none"
-                    stroke="url(#gradient2)"
-                    d="
-                M 195, 195
-                m 0, 193
-                a -193,-193 0 0,1 0,-386
-            "/>
-                <path
-                    ref={(path) => { svgFullPath = path }}
-                    strokeDasharray={1}
-                    strokeDashoffset={1}
-                    strokeWidth="4"
-                    strokeLinecap="round"
-                    fill="none"
-                    stroke="url(#gradient)"
-                    d="
-                M 195, 195
-                m 0, -193
-                a 193,193 0 0,1 0,386
-                a -193,-193 0 0,1 0,-386
-            "/>
+                        M 195, 195
+                        m 0, -193
+                        a 193,193 0 0,1 0,386
+                        a -193,-193 0 0,1 0,-386
+                    "/>
             </svg>
-            <div style={{transform: `translate(${state.indicator.x}px, ${state.indicator.y}px)`}} className={css(styles.indicator)}>
+            <div style={{transform: `translate(${state.indicator.x}px, ${state.indicator.y}px)`}} className={css(styles.indicator, styles.drag, state.isDragging && styles.pause)}>
                 <svg
-                    id="oneOff"
                     width="36px"
                     height="36px"
                     viewBox="0 0 36 36"
                     onMouseDown={onMouseDown}
-                    className={css(styles.animation)}
                 >
-                    <circle fill="#5C4BEA" fillOpacity="0.2" stroke="none" cx="18" cy="18" r="18"/>
-                    <circle fill="#5C4BEA" stroke="none" cx="18" cy="18" r="12"/>
-                    <rect fill="#FFFFFF" x="13" y="14" width="9" height="1"/>
-                    <rect fill="#FFFFFF" x="13" y="17" width="9" height="1"/>
-                    <rect fill="#FFFFFF" x="13" y="20" width="9" height="1"/>
+                    <circle fill="#5C4BEA" fillOpacity="0.2" stroke="none" cx="18" cy="18" r="18" className={css(!state.isDragging && styles.animation)} />
+                    <circle fill="#5C4BEA" stroke="none" cx="18" cy="18" r="12" />
+                    <rect fill="#FFFFFF" x="13" y="14" width="9" height="1" />
+                    <rect fill="#FFFFFF" x="13" y="17" width="9" height="1" />
+                    <rect fill="#FFFFFF" x="13" y="20" width="9" height="1" />
                 </svg>
             </div>
-            <p>Dragging: {state.isDragging ? 'true' : 'false'}</p>
         </div>
     );
 };
