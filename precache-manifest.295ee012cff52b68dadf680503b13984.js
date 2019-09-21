ea251ad826699ae9cecebdb26e549e3e595fa188
Fidel Seehawer <fidel@kreativgebiet.com>
2019-09-21 12:52:32 +0200

    Update to relative paths

---
+++ precache-manifest.295ee012cff52b68dadf680503b13984.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6492be89d56d7cf0e64b64ec1cafec6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d886df2f5ec2cbcdb8c8",
+    "url": "./static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d24a918f0f537a69dbc0",
+    "url": "./static/js/2.2d1eb385.chunk.js"
+  },
+  {
+    "revision": "d886df2f5ec2cbcdb8c8",
+    "url": "./static/js/main.9d3d1994.chunk.js"
+  },
+  {
+    "revision": "8a1d6b86fd672d575a6f",
+    "url": "./static/js/runtime~main.19313260.js"
+  }
+]);
