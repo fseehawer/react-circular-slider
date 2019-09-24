f9096dbd590c8b0279ecb7ba81b4a70c57658506
Fidel Seehawer <fidel@kreativgebiet.com>
2019-09-24 20:56:29 +0200

    Update package name

---
+++ build/precache-manifest.a35ac699c33147306d4575ac1eca9a54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35656009ab268e81b1b48b3931a7893",
+    "url": "/react-circular-slider/index.html"
+  },
+  {
+    "revision": "c50a9eee914ad85034c0",
+    "url": "/react-circular-slider/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ea7d6cb8706f2b5732ab",
+    "url": "/react-circular-slider/static/js/2.2a2647d3.chunk.js"
+  },
+  {
+    "revision": "c50a9eee914ad85034c0",
+    "url": "/react-circular-slider/static/js/main.80ab3508.chunk.js"
+  },
+  {
+    "revision": "ef5395d4da5dc246b061",
+    "url": "/react-circular-slider/static/js/runtime~main.d4b1d7d1.js"
+  }
+]);
