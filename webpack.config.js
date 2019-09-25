eacb9b846fe08dc9e70b6b1afd1e8cc8d160f9d6
Fidel Seehawer <fidel@kreativgebiet.com>
2019-09-25 10:28:48 +0200

    Fix npm module

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,32 @@
+var path = require('path');
 module.exports = {
+    entry: './src/CircularSlider/index.js',
+    output: {
+        path: path.resolve(__dirname),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2' // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+    },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /(node_modules|bower_components|build)/,
                 use: {
-                    loader: "babel-loader"
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                        plugins: ['transform-class-properties']
+                    }
                 }
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
             }
         ]
+    },
+    externals: {
+        'react': 'commonjs react' // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
     }
 };
