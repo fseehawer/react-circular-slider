4d793008f86d714a2da3c05ae245feb2da1cf0c7
Fidel Seehawer <fidel@fio.ai>
2019-09-30 19:54:41 +0200

    Move inline style to path class

--- precache-manifest.34c795d2e079197ae6c2298330afddc2.js
+++ precache-manifest.34c795d2e079197ae6c2298330afddc2.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ac6b149447dc7ee9654a42462bc88660",
+    "revision": "2af8984363a17fbaa543b19040690b5a",
     "url": "/react-circular-slider/index.html"
   },
   {
-    "revision": "29b2306231b89d0ad61c",
+    "revision": "fa58cf2d35a6f865ba1d",
     "url": "/react-circular-slider/static/css/main.654bcc83.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-circular-slider/static/js/2.b23f44af.chunk.js"
   },
   {
-    "revision": "29b2306231b89d0ad61c",
-    "url": "/react-circular-slider/static/js/main.3435449f.chunk.js"
+    "revision": "fa58cf2d35a6f865ba1d",
+    "url": "/react-circular-slider/static/js/main.866932d1.chunk.js"
   },
   {
     "revision": "62e2d8b9e145ed5f1dfd",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-circular-slider/precache-manifest.a5f07994669dc6ac295f2a4433a602b1.js"
+  "/react-circular-slider/precache-manifest.34c795d2e079197ae6c2298330afddc2.js"
 );
 
 self.addEventListener('message', (event) => {
