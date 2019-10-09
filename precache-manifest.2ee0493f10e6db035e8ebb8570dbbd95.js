a6229603d656882ed6523805f74a10743f483cd6
Fidel Seehawer <fidel@fio.ai>
2019-10-09 17:52:46 +0200

    Change how data is spread over x degrees

--- precache-manifest.2ee0493f10e6db035e8ebb8570dbbd95.js
+++ precache-manifest.2ee0493f10e6db035e8ebb8570dbbd95.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e74aecb9ea44dcd33d2b70dfd01dea00",
+    "revision": "e0a582bb660678f1ce7fa8dafd756740",
     "url": "/react-circular-slider/index.html"
   },
   {
-    "revision": "16974649e91db3f7bc12",
+    "revision": "0c6b071f1f18a244d383",
     "url": "/react-circular-slider/static/css/main.654bcc83.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-circular-slider/static/js/2.58e6d2f9.chunk.js"
   },
   {
-    "revision": "16974649e91db3f7bc12",
-    "url": "/react-circular-slider/static/js/main.f31e7fcb.chunk.js"
+    "revision": "0c6b071f1f18a244d383",
+    "url": "/react-circular-slider/static/js/main.3ad69d7b.chunk.js"
   },
   {
     "revision": "62e2d8b9e145ed5f1dfd",
