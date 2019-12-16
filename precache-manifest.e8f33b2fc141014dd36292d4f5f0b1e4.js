1c49955918d3e3a963ff10c4caa67cc59d55fbc3
Fidel Seehawer <fidel@fio.ai>
2019-12-16 14:01:56 +0100

    Fix progress rotation

--- precache-manifest.e8f33b2fc141014dd36292d4f5f0b1e4.js
+++ precache-manifest.e8f33b2fc141014dd36292d4f5f0b1e4.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "920f7c9f9b318ca081ec05e60682d352",
+    "revision": "937e061218a9aa3c84e3bc16e36c0f88",
     "url": "/react-circular-slider/index.html"
   },
   {
-    "revision": "637fefb5997d3d86ceac",
+    "revision": "6e9b8473b54e71de2697",
     "url": "/react-circular-slider/static/css/main.654bcc83.chunk.css"
   },
   {
@@ -12,19 +12,19 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-circular-slider/static/js/2.c7ec2044.chunk.js"
   },
   {
-    "revision": "637fefb5997d3d86ceac",
-    "url": "/react-circular-slider/static/js/main.d153f752.chunk.js"
+    "revision": "6e9b8473b54e71de2697",
+    "url": "/react-circular-slider/static/js/main.5b41d659.chunk.js"
   },
   {
     "revision": "12ff7b215f1fc306781e",
     "url": "/react-circular-slider/static/js/runtime~main.16929294.js"
   },
   {
-    "revision": "1949bc130b019568a1e974d4752708d9",
-    "url": "/react-circular-slider/static/media/emoji.1949bc13.svg"
+    "revision": "8c505251b42d39bf4f1a438d93c3d6f1",
+    "url": "/react-circular-slider/static/media/emoji.8c505251.svg"
   },
   {
-    "revision": "f697a14385242a25938fc381ad30a97d",
-    "url": "/react-circular-slider/static/media/power.f697a143.svg"
+    "revision": "8197afbc5e3d1a898d444624455ffaa1",
+    "url": "/react-circular-slider/static/media/power.8197afbc.svg"
   }
 ]);
